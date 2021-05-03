1b71a7e89ce76e23167a267fc45417188df2d690
chenqiulong <359790031@qq.com>
2021-05-03 20:03:58 +0800

    添加 css 编译命令

--- compile.js
+++ compile.js
@@ -7,5 +7,6 @@ const shell = require('shelljs');
 // 复制文件到lib目录
 shell.echo('Copy compiles files...');
 shell.cp('react-pixel/build/static/js/*.js', 'eruda-pixel/src/core.txt');
+shell.cp('react-pixel/build/static/css/*.css', 'eruda-pixel/src/style.txt');
 
 shell.echo('Copy successfully.');

12fa864ff1f1e38f91776cf308743dd225153e20
chenqiulong <359790031@qq.com>
2021-05-03 19:35:05 +0800

    feat: 添加构建

--- react-pixel/src/index.js
+++ react-pixel/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-console.log(object)
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function

cb42d87d85e689cd21c07059c2eb890c1891dfbf
chenqiulong <359790031@qq.com>
2021-05-04 12:00:21 +0800

    feat: 修改注释

--- compile.js
+++ compile.js
@@ -4,7 +4,7 @@
 
 const shell = require('shelljs');
 
-// 复制文件到lib目录
+// 复制 react-pixel/build/static/js 文件到 eruda-pixel/src 目录
 shell.echo('Copy compiles files...');
 shell.cp('react-pixel/build/static/js/*.js', 'eruda-pixel/src/core.txt');
 shell.cp('react-pixel/build/static/css/*.css', 'eruda-pixel/src/style.txt');
